61aa4141fa9e676366e70764c46ffbbba6a4341c
zpolwrek <zpolwrek413@hcc.edu>
2019-11-15 14:35:34 -0500

    Add route information to buses
    Color, RouteShortName, IvrDescription

--- pvta_cache.js
+++ pvta_cache.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 
 module.exports = class PVTACache {
   constructor(updateInterval = 180000) {
-    this.updateInterval = updateInterval;
+    this.updateInterval = updateInterval; // minimum time in ms between requests to the PVTA 
     this.lastUpdate = 0;
 
     this.routes = {};
@@ -48,18 +48,25 @@ module.exports = class PVTACache {
 
   // TODO: Handle errors for failed pvta fetch request
   async updateCache() {
+
+    // If it has been less then updateInterval ms since the last update, do not request new data
     if (Date.now() - this.lastUpdate < this.updateInterval) return;
 
     let start = Date.now();
     console.log("Cache out of date, updating data starting at " + start);
 
+    // Access pvta api for all route/bus/stop information
     let response = await fetch(
       "http://bustracker.pvta.com/InfoPoint/rest/routedetails/getallroutedetails"
     );
+
+    // Convert to JSON
     let json = await response.json();
 
     let c_start = Date.now();
 
+    // Go through each route we recevied from the PVTA and add the routes/stops/buses to
+    // individual associative arrays (dictionary)
     json.forEach(route => {
       this.routes[route.RouteId] = route;
 
@@ -68,6 +75,11 @@ module.exports = class PVTACache {
       });
 
       route.Vehicles.forEach(vehicle => {
+
+        vehicle.Color = route.Color;
+        vehicle.RouteShortName = route.ShortName;
+        vehicle.IvrDescription = route.IvrDescription;
+
         this.vehicles[vehicle.VehicleId] = vehicle;
       });
     });
@@ -75,6 +87,7 @@ module.exports = class PVTACache {
     let end = Date.now();
     console.log("Cache created, " + (end - c_start) + "ms");
 
+    // Set the lastUpdate time to the current time
     this.lastUpdate = Date.now();
   }
 };
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = require("express");
-const fetch = require("node-fetch");
+const express = require("express"); // REST API API
 const app = express();
 
+// Allows the use of system variable (in .env file)
 const dotenv = require("dotenv");
 dotenv.config();
 
+// Setup Google maps API with the API key in the .env file
 const googleMapsClient = require("@google/maps").createClient({
   key: process.env.GOOGLE_DIRECTIONS_API_KEY
 });
 
+// Use the port suppied by the system (Heroku) else default to port 3000
 const PORT = process.env.PORT || 3000;
 
 const PVTACache = require("./pvta_cache.js");
@@ -21,16 +23,30 @@ app.get("/", (req, res) => {
   res.send("Hi");
 });
 
+/*
+ *  returns all availiable PVTA routes
+ */
 app.get("/api/route/", (req, res) => {
+
+  // Get all route information from the PVTA cache
   pvta_cache.getRoutes().then(routes => {
+
+    // Send the route information, if not exists, send null
     res.send({
       Routes: routes || null
     });
   });
 });
 
+/*
+ *  returns a PVTA route with a route id = :route_id
+ *  Example: /api/route/20035  returns information on route 20035
+ */
 app.get("/api/route/:route_id", (req, res) => {
+
+  // Get the route information for route req.params.route_id from the PVTA cache
   pvta_cache.getRoute(req.params.route_id).then(route => {
+    // Send the route information, if not exists, send null
     res.send({
       Route: route || null
     });
@@ -85,8 +101,7 @@ app.get("/api/directions/", (req, res) => {
   console.log("From: " + req.query.from);
   console.log("To: " + req.query.to);
 
-  googleMapsClient.directions(
-    {
+  googleMapsClient.directions({
       origin: req.query.from,
       destination: req.query.to,
       traffic_model: "best_guess",
